1307c71f69cfff4e1cefbf5787d33c2ca6eca8d3
Alex Gorbach <a.v.gorbach@gmail.com>
2018-11-21 20:12:21 +1000

    Исправления в схеме базы данных по результатам ревью

--- db/create_db_l1.sql
+++ db/create_db_l1.sql
@@ -2,7 +2,7 @@ create database frontend_weekend
 	with owner postgres
 ;
 
-create table if not exists events
+create table events
 (
 	event_id serial not null
 		constraint events_pkey
@@ -18,69 +18,41 @@ create table if not exists events
 alter table events owner to postgres
 ;
 
-create table if not exists speakers
-(
-	speaker_id integer not null
-		constraint speakers_pkey
-			primary key,
-	name text not null,
-	photo text,
-	description text
-)
-;
-
-alter table speakers owner to postgres
-;
-
-create table if not exists presentations
+create table presentations
 (
 	presentation_id serial not null
 		constraint presentations_pkey
 			primary key,
 	name text not null,
 	description text,
-	images text[]
+	images text[],
+	event_id integer not null
+		constraint presentations_events_event_id_fk
+			references events
 )
 ;
 
 alter table presentations owner to postgres
 ;
 
-create table if not exists event_presentations
+create table speakers
 (
-	event_id integer
-		constraint event_presentations_events_event_id_fk
-			references events,
-	presentation_id integer
-		constraint event_presentations_presentations_presentation_id_fk
-			references presentations,
-	event_presentation_id serial not null
-		constraint event_presentations_pkey
-			primary key
+	speaker_id integer not null
+		constraint speakers_pkey
+			primary key,
+	name text not null,
+	photo text,
+	description text,
+	presentation_id integer not null
+		constraint speakers_presentations_presentation_id_fk
+			references presentations
 )
 ;
 
-alter table event_presentations owner to postgres
+alter table speakers owner to postgres
 ;
 
-create table if not exists presentation_speakers
-(
-	presentation_speaker_id serial not null
-		constraint presentation_speakers_pkey
-			primary key
-		constraint presentation_speakers_presentations_presentation_id_fk
-			references presentations,
-	presentation_id integer,
-	speaker_id integer
-		constraint presentation_speakers_speakers_speaker_id_fk
-			references speakers
-)
-;
-
-alter table presentation_speakers owner to postgres
-;
-
-create table if not exists comments
+create table comments
 (
 	comment_id serial not null
 		constraint comments_pkey
@@ -92,7 +64,7 @@ create table if not exists comments
 alter table comments owner to postgres
 ;
 
-create table if not exists ratings
+create table ratings
 (
 	rating_id serial not null
 		constraint ratings_pkey
@@ -104,105 +76,123 @@ create table if not exists ratings
 alter table ratings owner to postgres
 ;
 
-create table if not exists event_ratings
+create table event_ratings
 (
-	event_rating_id serial not null
-		constraint event_ratings_pkey
-			primary key,
 	event_id integer not null
 		constraint event_ratings_events_event_id_fk
 			references events,
 	rating_id integer not null
 		constraint event_ratings_ratings_rating_id_fk
-			references ratings
+			references ratings,
+	constraint event_ratings_pk
+		primary key (event_id, rating_id)
 )
 ;
 
 alter table event_ratings owner to postgres
 ;
 
-create table if not exists presentation_ratings
+create unique index event_ratings_rating_id_uindex
+	on event_ratings (rating_id)
+;
+
+create table presentation_ratings
 (
-	presentation_rating_id serial not null
-		constraint presentation_ratings_pkey
-			primary key,
 	rating_id integer not null
 		constraint presentation_ratings_ratings_rating_id_fk
 			references ratings,
 	presentation_id integer not null
 		constraint presentation_ratings_presentations_presentation_id_fk
-			references presentations
+			references presentations,
+	constraint presentation_ratings_pk
+		primary key (rating_id, presentation_id)
 )
 ;
 
 alter table presentation_ratings owner to postgres
 ;
 
-create table if not exists speaker_ratings
+create unique index presentation_ratings_rating_id_uindex
+	on presentation_ratings (rating_id)
+;
+
+create table speaker_ratings
 (
-	speaker_rating_id serial not null
-		constraint speaker_ratings_pkey
-			primary key,
 	rating_id integer not null
 		constraint speaker_ratings_ratings_rating_id_fk
 			references ratings,
 	speaker_id integer not null
 		constraint speaker_ratings_speakers_speaker_id_fk
-			references speakers
+			references speakers,
+	constraint speaker_ratings_pk
+		primary key (rating_id, speaker_id)
 )
 ;
 
 alter table speaker_ratings owner to postgres
 ;
 
-create table if not exists event_comments
+create unique index speaker_ratings_rating_id_uindex
+	on speaker_ratings (rating_id)
+;
+
+create table event_comments
 (
-	event_comment_id serial not null
-		constraint event_comments_pkey
-			primary key,
 	comment_id integer not null
 		constraint event_comments_comments_comment_id_fk
 			references comments,
 	event_id integer not null
 		constraint event_comments_events_event_id_fk
-			references events
+			references events,
+	constraint event_comments_pk
+		primary key (comment_id, event_id)
 )
 ;
 
 alter table event_comments owner to postgres
 ;
 
-create table if not exists presentation_comments
+create unique index event_comments_comment_id_uindex
+	on event_comments (comment_id)
+;
+
+create table presentation_comments
 (
-	presentation_comment_id serial not null
-		constraint presentation_comments_pkey
-			primary key,
 	comment_id integer not null
 		constraint presentation_comments_comments_comment_id_fk
 			references comments,
 	presentation_id integer not null
 		constraint presentation_comments_presentations_presentation_id_fk
-			references presentations
+			references presentations,
+	constraint presentation_comments_pk
+		primary key (comment_id, presentation_id)
 )
 ;
 
 alter table presentation_comments owner to postgres
 ;
 
-create table if not exists speaker_comments
+create unique index presentation_comments_comment_id_uindex
+	on presentation_comments (comment_id)
+;
+
+create table speaker_comments
 (
-	speaker_comment_id serial not null
-		constraint speaker_comments_pkey
-			primary key,
 	comment_id integer not null
 		constraint speaker_comments_comments_comment_id_fk
 			references comments,
 	speaker_id integer not null
 		constraint speaker_comments_speakers_speaker_id_fk
-			references speakers
+			references speakers,
+	constraint speaker_comments_pk
+		primary key (comment_id, speaker_id)
 )
 ;
 
 alter table speaker_comments owner to postgres
+;
+
+create unique index speaker_comments_comment_id_uindex
+	on speaker_comments (comment_id)
 ;
 
